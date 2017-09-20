e0ad1e8cc785424ba87185298173139c51571029
antiseptix <jean.still68@yahoo.fr>
2017-09-21 00:35:17 +0200

    Add GEntite and IEntite

---
+++ Zoom/Zoom/Gestionnaire/GEntite.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zoom.BLL.Model;
+
+namespace Zoom.BLL.Gestionnaire
+{
+    /**
+     * Classe static gestionnaire de toutes les entités de notre zoo
+     */ 
+    public class GEntite<T> where T : IEntite
+    {
+        private static List<T> listeEntite = new List<T>();
+
+        public static List<T> ListeEntite
+        {
+            get { return listeEntite; }
+            set { listeEntite  = value; }
+        }
+
+        public static void Add(T entite)
+        {
+            listeEntite.Add(entite);
+        }
+
+        public static void Remove(T entite)
+        {
+            listeEntite.Remove(entite);
+        }
+
+        public static List<T> GetOnebyID(int id)
+        {
+            return ListeEntite.Find(x=> x.Id == id);
+        }
+
+        public static List<T> GetAll()
+        {
+            return ListeEntite;
+        }
+    }
+}
